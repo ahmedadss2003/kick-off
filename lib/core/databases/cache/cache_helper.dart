31b56dba9e6c684c81a3a852aa83399330c3d6fb
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2025-12-30 14:48:08 +0200

    add core files

---
+++ lib/core/databases/cache/cache_helper.dart
@@ -0,0 +1,57 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheHelper {
+  static SharedPreferences? _preferences;
+
+  /// Initialize SharedPreferences
+  static Future<void> init() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  /// Save data to SharedPreferences
+  static Future<void> setData<T>(String key, T value) async {
+    if (_preferences == null)
+      throw Exception("SharedPreferences not initialized");
+
+    if (value is String) {
+      await _preferences!.setString(key, value);
+    } else if (value is int) {
+      await _preferences!.setInt(key, value);
+    } else if (value is bool) {
+      await _preferences!.setBool(key, value);
+    } else if (value is double) {
+      await _preferences!.setDouble(key, value);
+    } else if (value is List<String>) {
+      await _preferences!.setStringList(key, value);
+    } else {
+      throw Exception("Unsupported type");
+    }
+  }
+
+  /// Retrieve data from SharedPreferences
+  static T? getData<T>(String key) {
+    if (_preferences == null)
+      throw Exception("SharedPreferences not initialized");
+
+    if (T == String) {
+      return _preferences!.getString(key) as T?;
+    } else if (T == int) {
+      return _preferences!.getInt(key) as T?;
+    } else if (T == bool) {
+      return _preferences!.getBool(key) as T?;
+    } else if (T == double) {
+      return _preferences!.getDouble(key) as T?;
+    } else if (T == List<String>) {
+      return _preferences!.getStringList(key) as T?;
+    } else {
+      throw Exception("Unsupported type");
+    }
+  }
+
+  /// Remove data from SharedPreferences
+  static Future<void> removeData(String key) async {
+    if (_preferences == null)
+      throw Exception("SharedPreferences not initialized");
+    await _preferences!.remove(key);
+  }
+}
---
+++ lib/core/routes_manager/routes_generators.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kickoff/main.dart';
+
+import 'routes.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.main:
+        return MaterialPageRoute(
+          builder: (_) => const MyHomePage(title: 'test'),
+        );
+
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: const Text('No Route Found')),
+        body: const Center(child: Text('No Route Found')),
+      ),
+    );
+  }
+}
