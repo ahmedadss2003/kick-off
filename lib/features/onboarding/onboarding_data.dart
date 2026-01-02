3cfb1d687e9b4101ef362b413e42c39e376a6c04
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2026-01-02 23:12:16 +0200

    Add SplashScreen with logo and text animation
    - Implemented SplashScreen as a StatefulWidget.
    - Added logo fade-in and scale animation using AnimationController.
    - Introduced text animation to display the word "kickoff" letter by letter.
    - Navigated to OnboardingScreen after animations complete.
    - Included assets for the logo image.

--- lib/features/onboarding/onboarding_data.dart
+++ lib/features/onboarding/onboarding_data.dart
@@ -1,16 +1,14 @@
-import 'package:flutter/material.dart';
-
 const List<Map<String, dynamic>> onboardingPages = [
   {
-    'title': 'Your Game,\n OnTap Away',
+    'title': 'Your Game,\n One Tap Away',
     'description':
         'Secure your spot at the arena instantly,\nNo calls, no waiting — just football.',
-    'icon': Icons.waving_hand,
+    'image': 'assets/images/kickoff_onboaring.png',
   },
   {
     'title': 'Secure Your Slot Instantly',
     'description':
         'No more phone calls.\nCheck real-time availability and book the field with a single tap.',
-    'icon': Icons.explore,
+    'image': 'assets/images/kickoff_onboardingll.png',
   },
 ];
