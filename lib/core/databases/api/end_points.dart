2174a3f92ab036fdb8bdd47c88d807835c36332e
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2026-03-16 02:47:43 +0200

    edit error model and endpoint

--- lib/core/databases/api/end_points.dart
+++ lib/core/databases/api/end_points.dart
@@ -1,9 +1,20 @@
 class EndPoints {
   static const String baserUrl = "https://kickoff.itravel2egypt.com/api/";
+  static const String imageBaseUrl = "https://kickoff.itravel2egypt.com/";
   static const String register = "register";
 
   static const String login = "login";
   static const String logout = "logout";
   static const String stadiums = "user/fields";
   static const String profile = "user/profile";
+
+  static String reviews = "user/revies/create";
+  static String getReviews(int fieldId) => "user/revies/$fieldId";
+  static String updateReview(int reviewId) => "user/revies/$reviewId";
+  static String deleteReview(int reviewId) => "user/revies/$reviewId";
+
+  static String addReply = "user/replies/create";
+  static String getReplies(int reviewId) => "user/replies/$reviewId";
+  static String updateReply(int replyId) => "user/replies/$replyId";
+  static String deleteReply(int replyId) => "user/replies/$replyId";
 }
